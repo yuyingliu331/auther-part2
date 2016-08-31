15949ef4962f4793070e88561bd045fc68f7fcaf
yuyingliu331 <liuyuying90@live.cn>
2016-08-31 18:20:32 -0400

    initial commit

---
+++ server/api/users/user.model.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var Sequelize = require('sequelize');
+
+var db = require('../../_db');
+
+var User = db.define('user', {
+  name: Sequelize.STRING,
+  photo: {
+    type: Sequelize.STRING,
+    defaultValue: '/images/default-photo.jpg'
+  },
+  phone: Sequelize.STRING,
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true
+    }
+  },
+  password: Sequelize.STRING,
+  isAdmin: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false
+  },
+  googleId: Sequelize.STRING,
+  twitterId: Sequelize.STRING,
+  githubId: Sequelize.STRING
+});
+
+module.exports = User;
---
+++ server/auth/google.oauth.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var router = require('express').Router();
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+var secrets = require('../../secrets.json');
+
+var User = require('../api/users/user.model');
+
+router.get('/', passport.authenticate('google', {
+  scope: 'email'
+}));
+
+router.get('/callback', passport.authenticate('google', {
+  successRedirect: '/stories',
+  failureRedirect: '/signup'
+}));
+
+passport.use(new GoogleStrategy({
+  clientID: '238524570915-ivf9lnhm9bsfq13cle5ap8s28d4lmhrp.apps.googleusercontent.com',
+  clientSecret: secrets.google,
+  callbackURL: 'http://127.0.0.1:8080/auth/google/callback'
+}, function (token, refreshToken, profile, done) {
+  var info = {
+    name: profile.displayName,
+    // google may not provide an email, if so we'll just fake it
+    email: profile.emails ? profile.emails[0].value : [profile.username , 'fake-auther-email.com'].join('@'),
+    photo: profile.photos ? profile.photos[0].value : undefined
+  };
+  User.findOrCreate({
+    where: {googleId: profile.id},
+    defaults: info
+  })
+  .spread(function (user) {
+    done(null, user);
+  })
+  .catch(done);
+}));
+
+module.exports = router;
